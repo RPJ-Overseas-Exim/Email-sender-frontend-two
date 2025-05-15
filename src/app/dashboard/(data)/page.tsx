4e4895188623f9c825c8106128392e0758aafba8
Vikrant <tiwarivikrant9764@gmail.com>
2025-05-15 21:47:46 +0530

    resolved linting error

--- src/app/dashboard/(data)/page.tsx
+++ src/app/dashboard/(data)/page.tsx
@@ -6,91 +6,91 @@ import ThemeSwitch from "@/components/context/ThemeSwitch";
 import ImportExport from "@/components/dashboard/dataEditor/ImportExport";
 export const dynamic = "force-dynamic";
 export default async function DataEditor({
-  searchParams,
+    searchParams,
 }: {
-  searchParams: { [x: string]: string };
+    searchParams: { [x: string]: string };
 }) {
-  const { data, count } = await applyFilters(searchParams);
-  return (
-    <section id="data-editor" className="data-editor mx-auto w-[96%]">
-      <div className="flex items-center justify-between">
-        <div className="flex items-center gap-2">
-          <h1 className="sidebar__title py-8" style={{ display: "block" }}>
-            Data Editor
-          </h1>
-          <div className="lg:hidden">
-            <ThemeSwitch />
-          </div>
-        </div>
-        <ImportExport type={searchParams["type"] || ""} limit={Number(count)} />
-      </div>
-      <SelectTable data={data} count={Number(count)} />
-    </section>
-  );
+    const { data, count } = await applyFilters(searchParams);
+    return (
+        <section id="data-editor" className="data-editor mx-auto w-[96%]">
+            <div className="flex items-center justify-between">
+                <div className="flex items-center gap-2">
+                    <h1 className="sidebar__title py-8" style={{ display: "block" }}>
+                        Data Editor
+                    </h1>
+                    <div className="lg:hidden">
+                        <ThemeSwitch />
+                    </div>
+                </div>
+                <ImportExport limit={Number(count)} />
+            </div>
+            <SelectTable data={data} count={Number(count)} />
+        </section>
+    );
 }
 
 function extractData(
-  resData: { [x: string]: string }[],
-  sentDateFilter: { startDate: string | null; endDate: string | null },
+    resData: { [x: string]: string }[],
+    sentDateFilter: { startDate: string | null; endDate: string | null },
 ) {
-  let customers: Customer[] = [];
-  let startDate = null,
-    endDate = null;
-  if (sentDateFilter.startDate)
-    startDate = new Date(sentDateFilter.startDate).toLocaleDateString();
-  if (sentDateFilter.endDate)
-    endDate = new Date(sentDateFilter.endDate).toLocaleDateString();
+    let customers: Customer[] = [];
+    let startDate = null,
+        endDate = null;
+    if (sentDateFilter.startDate)
+        startDate = new Date(sentDateFilter.startDate).toLocaleDateString();
+    if (sentDateFilter.endDate)
+        endDate = new Date(sentDateFilter.endDate).toLocaleDateString();
 
-  resData.map((customer: { [x: string]: string }) => {
-    const { id, name, email, product, number } = customer;
-    let { sentDate } = customer;
+    resData.map((customer: { [x: string]: string }) => {
+        const { id, name, email, product, number } = customer;
+        let { sentDate } = customer;
 
-    let status: "sent" | "pending" = "pending";
-    const sentDateString = new Date(sentDate).toLocaleDateString();
-    const currDate = new Date().toLocaleDateString();
+        let status: "sent" | "pending" = "pending";
+        const sentDateString = new Date(sentDate).toLocaleDateString();
+        const currDate = new Date().toLocaleDateString();
 
-    if (startDate && sentDateString >= startDate) {
-      status = "sent";
-    }
-    if (endDate && sentDateString <= endDate) {
-      status = "sent";
-    }
-    if (sentDateString === currDate) status = "sent";
+        if (startDate && sentDateString >= startDate) {
+            status = "sent";
+        }
+        if (endDate && sentDateString <= endDate) {
+            status = "sent";
+        }
+        if (sentDateString === currDate) status = "sent";
 
-    customers.push({
-      id,
-      name,
-      email,
-      productId: product,
-      status: status,
-      number: number,
+        customers.push({
+            id,
+            name,
+            email,
+            productId: product,
+            status: status,
+            number: number,
+        });
     });
-  });
-  return customers;
+    return customers;
 }
 
 async function applyFilters(searchParams: { [x: string]: string }) {
-  let data: Customer[];
-  let count = 0;
+    let data: Customer[];
+    let count = 0;
 
-  try {
-    const queryString = new URLSearchParams(searchParams);
-    let res;
-    if (queryString.get("startDate") || queryString.get("endDate")) {
-      res = await GetRequest("/stats?" + queryString.toString());
-    } else res = await GetRequest("/customers?" + queryString.toString());
-    if (res?.data && res?.count) {
-      data = extractData(res.data, {
-        endDate: searchParams["endDate"],
-        startDate: searchParams["startDate"],
-      });
-      count = res.count;
-      return { data, count };
-    } else {
-      return { data: [], count };
+    try {
+        const queryString = new URLSearchParams(searchParams);
+        let res;
+        if (queryString.get("startDate") || queryString.get("endDate")) {
+            res = await GetRequest("/stats?" + queryString.toString());
+        } else res = await GetRequest("/customers?" + queryString.toString());
+        if (res?.data && res?.count) {
+            data = extractData(res.data, {
+                endDate: searchParams["endDate"],
+                startDate: searchParams["startDate"],
+            });
+            count = res.count;
+            return { data, count };
+        } else {
+            return { data: [], count };
+        }
+    } catch (error) {
+        console.log(error);
     }
-  } catch (error) {
-    console.log(error);
-  }
-  return { data: null, count };
+    return { data: null, count };
 }
--- src/components/dashboard/dataEditor/ImportExport.tsx
+++ src/components/dashboard/dataEditor/ImportExport.tsx
@@ -3,16 +3,16 @@ import ImportCSV from "@/components/dashboard/dataEditor/ImportCSV";
 import ExportCSV from "@/components/dashboard/dataEditor/ExportCSV";
 import useAuth from "@/components/context/AuthProvider";
 
-export default function ImportExport({ limit, type }: { limit:number, type:string }) {
-  const { auth } = useAuth();
-  return (
-    <>
-      {auth?.role === "admin" && (
-        <div className="flex items-center gap-1 lg:gap-4">
-          <ImportCSV />
-          <ExportCSV limit={limit} type={type} />
-        </div>
-      )}
-    </>
-  );
+export default function ImportExport({ limit }: { limit: number }) {
+    const { auth } = useAuth();
+    return (
+        <>
+            {auth?.role === "admin" && (
+                <div className="flex items-center gap-1 lg:gap-4">
+                    <ImportCSV />
+                    <ExportCSV limit={limit} />
+                </div>
+            )}
+        </>
+    );
 }
